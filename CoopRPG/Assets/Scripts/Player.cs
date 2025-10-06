d349b2115078f3b2c01c8d0841cf5225832ab7b8
TheBlaze <santi.volcanos@gmail.com>
2025-10-06 09:58:57 -0600

    shoot added

--- CoopRPG/Assets/Scripts/Player.cs
+++ CoopRPG/Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class Player : NetworkBehaviour
 {
     private InputActions inputActions;
-
+    bool eggpain = false;
+    bool fayah = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -14,13 +15,30 @@ public class Player : NetworkBehaviour
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (!eggpain)
+        {
+            eggpain = inputActions.Player.Action.triggered;
+        }
+
+        if (!fayah)
+        {
+            fayah = inputActions.Player.Shoot.triggered;
+        }
+    }
     public override void FixedUpdateNetwork()
     {
         base.FixedUpdateNetwork();
-        bool foo = inputActions.Player.Action.ReadValue<float>() > 0;
-        if (Object.HasInputAuthority && foo == true)
+        if (Object.HasInputAuthority && eggpain == true)
         {
+            eggpain = false;
             RPC_CallTraficLight();
+        }
+        if (Object.HasInputAuthority && fayah == true)
+        {
+            fayah = false;
+            Rpc_ShootAShot();
         }
     }
 
@@ -29,4 +47,24 @@ public class Player : NetworkBehaviour
     {
         ObjectManager.singleton.trafficLight.ChangeColor();
     }
+    #region PewPew
+    public GameObject bulletPrefab;
+    public Transform shootPos;
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    private void Rpc_ShootAShot()
+    {
+        var bullet = Runner.Spawn(bulletPrefab, shootPos.position, shootPos.rotation, Object.InputAuthority);
+
+        if (bullet.TryGetComponent(out Rigidbody rb))
+        {
+            rb.AddForce(bullet.transform.forward * 3, ForceMode.Impulse);
+        }
+        else
+        {
+            Debug.Log("yo cuando no tengo cuerpo");
+        }
+    }
+    #endregion
+
 }
